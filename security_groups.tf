38d9b3f04011332f35db9b9528acea1b3c09128f
Likhitha Tangudu <86105341+Likhitha-03@users.noreply.github.com>
2024-10-29 14:16:49 +0530

    Create security_groups.tf

---
+++ security_groups.tf
@@ -0,0 +1,98 @@
+#security group for application load balancer
+resource "aws_security_group" "alb_sg_likky" {
+  name        = "alb_sg_likky"
+  description = "Security group for Application Load Balancer"
+  vpc_id      = aws_vpc.wordpress_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = [
+      ingress,egress
+    ]
+  }
+}
+
+#security group for the ec2 instance
+resource "aws_security_group" "wordpress_sg_likky" {
+  name        = "wordpress_sg_likky"
+  description = "Security group for WordPress EC2 instance"
+  vpc_id      = aws_vpc.wordpress_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["49.205.251.148/32"]  
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    ignore_changes = [ingress,egress]
+  }
+}
+
+#security group for the rds instance
+resource "aws_security_group" "rds_sg_likky" {
+  name        = "rds_sg_likky"
+  description = "Security group for RDS instance"
+  vpc_id      = aws_vpc.wordpress_vpc.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.wordpress_sg_likky.id]  
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
